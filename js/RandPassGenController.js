3d8dc75f5f0e887c7a4c9d37fa9c40f4a3e6b8a6
gjanden <gjanden@nudle.co>
2014-09-29 14:11:49 -0600

    connected checkbox values to javascript variables

--- js/RandPassGenController.js
+++
@@ -1,4 +0,0 @@
-app.controller('RandPassGenController', function($scope){
-
-
-});
--- js/services.js
+++ js/services.js
@@ -4,13 +4,14 @@ $(document).ready(function(){
 	var lowNum = 8;
 	var highNum = 128;
 	var charNum = [];
+	var apostrophe = "'";	
+//	var characterSets = {
 	var lowerCase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 	var upperCase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
-	var nums = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-	var apostrophe = "'";
 	var special = [' ','!','@','#','$','%','^','&','*','(',')','-','_','=','+','[','{',']','}','|',';',':','"','<',',','>','.','?','/','\\',apostrophe];
-	var simChar = ['1','!','i','I','o','O','0','|'];
-	var randArray = [lowerCase, upperCase, nums, special];
+	var nums = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+//	};
+	var randArray = [];
 	var passLength = $('#passLength');
 
 	var length = document.getElementById("passLength");
@@ -20,12 +21,13 @@ $(document).ready(function(){
 
 	var genBtn = $('#passGenBtn');
 
+
 // Pushes values into the charNum array
 	while(lowNum < highNum + 1){
 	  charNum.push(lowNum++);
 	};
 
-// Populates #passLength select element with option values from chrNum array
+// Populates #passLength select element with option values from charNum array
 	window.onload = function() {
 	    var select = document.getElementById('passLength');
 	    var options = charNum;
@@ -38,20 +40,51 @@ $(document).ready(function(){
 	    }
 	};
 
+// Function to push array values into newArray[]	
+	$('input:checkbox').change(function(){
+		if($(this).is(':checked') && $(this).val() === 'special') {
+			randArray.push(special);
+		} else if ($(this).is(':checked') && $(this).val() === 'lowercase') {
+			randArray.push(lowerCase);
+		} else if ($(this).is(':checked') && $(this).val() === 'uppercase') {
+			randArray.push(upperCase);
+		} else if ($(this).is(':checked') && $(this).val() === 'nums') {
+			randArray.push(nums);
+		} 
+	});
+
+// Function to splice array values from newArray[]	
+		$('input:checkbox').change(function(){
+		if(!$(this).is(':checked') && $(this).val() === 'special') {
+			randArray.splice(randArray.indexOf(special), 1);
+		} else if (!$(this).is(':checked') && $(this).val() === 'lowercase') {
+			randArray.splice(randArray.indexOf(lowerCase), 1);
+		} else if (!$(this).is(':checked') && $(this).val() === 'uppercase') {
+			randArray.splice(randArray.indexOf(upperCase), 1);
+		} else if (!$(this).is(':checked') && $(this).val() === 'nums') {
+			randArray.splice(randArray.indexOf(nums), 1);
+		} 
+		console.log(randArray)
+	});	
+
+
 // Loop that populates #output with random characters from random arrays
 	genBtn.on('click', function() {
-			randPassArray.length = 0;
-			for(var i = 0; i < passLength.val(); i ++){
-				var rand = randArray[Math.floor(Math.random() * randArray.length)];
-				var randChar = rand[Math.floor(Math.random() * rand.length)];
-				randPassArray.push(randChar);
-				var myPass = randPassArray.join('');
-				$('#output').val(myPass);
+		if(randArray.length > 0){
+				randPassArray.length = 0;
+				for(var i = 0; i < passLength.val(); i ++){
+					var rand = randArray[Math.floor(Math.random() * randArray.length)];
+					var randChar = rand[Math.floor(Math.random() * rand.length)];
+					randPassArray.push(randChar);
+					var myPass = randPassArray.join('');
+					$('#output').val(myPass);
+				}
+			} else {
+				alert('You must select at least one character type.');
 			}
-			return randPassArray	
+			return randPassArray;
+			console.log(randArray);
 	});	
+//	console.log(characterSets);
 });
-
-
-
 
